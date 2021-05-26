3894d16c39af1bc38b5af8977b57b3cc996c9c3d
Yeowoony <dbd04009@naver.com>
2021-05-26 11:16:18 +0900

    add fontawesome icons

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
           운동(헬스)하는 사람을 위한 웹사이트
         </p>
         <p class="subtitle">
-          운동 부위
+          <i class="fas fa-dumbbell"></i>운동 <i class="fas fa-child"></i>부위
         </p>
       </div>
     </section>
