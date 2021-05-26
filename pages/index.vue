e4d16c38d65d453d8802d25916dcc16f50b1edf6
Yeowoony <dbd04009@naver.com>
2021-05-26 12:19:57 +0900

    add Exercise routine photos

--- pages/index.vue
+++ pages/index.vue
@@ -6,8 +6,228 @@
           운동(헬스)하는 사람을 위한 웹사이트
         </p>
         <p class="subtitle">
-          <i class="fas fa-dumbbell"></i>운동 <i class="fas fa-child"></i>부위
+          <i class="fas fa-dumbbell"></i>운동 
+          <i class="fas fa-child"></i>부위
         </p>
+      </div>
+    </section>
+    <section class="columns">
+      <div class="column">
+        <article class="message">
+          <div class="message-header">
+            <p>가슴</p>
+          </div>
+          <div class="message-body">
+            <figure class="image">
+              <img
+                src="https://mblogthumb-phinf.pstatic.net/20150402_213/nabuilder_1427979616482YttWd_JPEG/chestpress.jpg?type=w2"
+                alt=""
+              />
+            </figure>
+
+체스트 프레스 머신
+
+12~15회
+
+4set
+
+<figure class="image">
+  <img
+    src="https://mblogthumb-phinf.pstatic.net/20150402_25/nabuilder_1427979790619UF40c_JPEG/incline.jpg?type=w2"
+    alt=""
+  />
+</figure>
+
+인클라인 체스트 프레스 
+
+머신
+
+12~15회
+
+4set
+
+<figure class="image">
+  <img
+    src="https://mblogthumb-phinf.pstatic.net/20150402_108/nabuilder_1427980211726hUOz2_JPEG/butterfly.jpg?type=w2"
+    alt=""
+  />
+</figure>
+
+버터플라이 머신
+
+12~15회
+
+3set
+
+          </div>
+        </article>
+      </div>
+      <div class="column">
+        <article class="message">
+          <div class="message-header">
+            <p>등</p>
+          </div>
+          <div class="message-body">
+              <figure class="image">
+                <img
+                  src="https://mblogthumb-phinf.pstatic.net/20150411_147/nabuilder_1428753832342Kr5m9_JPEG/wide-grip-lat-pulldown1.jpg?type=w2"
+                  alt=""
+                  />
+              </figure>
+
+              랫 풀 다운
+
+              12~15회
+              
+              4set
+              
+<figure class="image">
+                <img
+                  src="https://mblogthumb-phinf.pstatic.net/20150411_210/nabuilder_1428755817781RmxDI_JPEG/pull-down-beh-neck2.jpg?type=w2"
+                  alt=""
+                  />
+              </figure>
+              
+              비하인드 넥 풀 다운
+              
+              12~15회
+              
+              4set
+              
+              <figure class="image">
+                <img
+                  src="https://mblogthumb-phinf.pstatic.net/20150405_93/nabuilder_1428203066986Hu4As_JPEG/seated_cable_row.jpg?type=w2"
+                  alt=""
+                  />
+              </figure>
+              
+              시티드 케이블 로우
+              
+              12~15회
+              
+              3set
+              
+
+          </div>
+        </article>
+      </div>
+      <div class="column">
+        <article class="message">
+          <div class="message-header">
+            <p>하체</p>
+          </div>
+          <div class="message-body">
+            <figure class="image">
+              <img
+                src="https://mblogthumb-phinf.pstatic.net/20150324_153/nabuilder_1427198770853LDLBx_JPEG/girls-squat-.14.jpg?type=w2"
+                alt=""
+                />
+            </figure>
+              스쿼트
+
+ 12~15회
+
+ 4set
+
+ <figure class="image">
+  <img
+    src="https://t1.daumcdn.net/cfile/tistory/99A92E4D5AD9CAAE36"
+    alt=""
+    />
+</figure>
+
+ 레그 익스텐션
+
+ 12~15회
+
+ 3set
+
+ <figure class="image">
+  <img
+    src="https://blog.kakaocdn.net/dn/oKOOl/btqz7pzhAue/72rC9wcsIXJC3b76kAuwuk/img.jpg"
+    alt=""
+    />
+</figure>
+
+ 레그 컬
+
+ 12~15회
+
+ 3set
+
+
+          </div>
+        </article>
+      </div>
+      <div class="column">
+        <article class="message">
+          <div class="message-header">
+            <p>어깨</p>
+          </div>
+          <div class="message-body">
+              <figure class="image">
+                <img
+                  src="https://t1.daumcdn.net/cfile/blog/99F43B365B30A63709"
+                  alt=""
+                />
+              </figure>
+              덤벨 숄더프레스
+
+              12~15회
+              
+              4set
+              
+              <figure class="image">
+                <img
+                  src="https://img1.daumcdn.net/thumb/R720x0.q80/?scode=mtistory2&fname=http%3A%2F%2Fcfile23.uf.tistory.com%2Fimage%2F99677F335ADB3CC3311565"
+                  alt=""
+                />
+              </figure>
+              
+              덤벨 사이드 레터럴레이즈
+              
+              12~15회
+              
+              3set
+              
+              <figure class="image">
+                <img
+                  src="https://mblogthumb-phinf.pstatic.net/MjAxODA2MTRfMjE4/MDAxNTI4OTQ5MzYzMzc4.5CjISR1T4qInO9qyVHmND5qgJKeHihNNGt2DL7BfIkUg.UymQawZX7AdRWE6rLvzCY-Y-atJeyXGu80tKtsHyfx8g.PNG.skathf22/1.png?type=w800"
+                  alt=""
+                />
+              </figure>
+              
+              벤트 오버 레터럴레이즈
+              
+              12~15회
+              
+              3set
+              
+              
+          </div>
+        </article>
+      </div>
+      <div class="column">
+        <article class="message">
+          <div class="message-header">
+            <p>유산소</p>
+          </div>
+          <div class="message-body">
+              <figure class="image">
+                <img
+                  src="https://clroro.com/wp-content/uploads/2020/08/50.webp"
+                  alt=""
+                />
+              </figure>
+              
+            <p>
+              유산소성 운동에 대한 루틴은 주 3~4회 / 20분내외가 적당합니다.<br><br>
+              요새 유산소성 운동은 효과가 좋은 인터벌 트레이닝, 서킷 트레이닝이 유행하고 있습니다.<br><br>
+              매일 매일 두 가지 운동을 소화하실 시간이 없으신 직장인 분들은 격일로 시행해도됩니다.<br><br>
+              그러나 오전 오후로 하루에 2번 실시하시는 것은 비추입니다. 몸은 회복할 시간이 필요합니다.
+            </p>
+          </div>
+        </article>
       </div>
     </section>
   </div>
