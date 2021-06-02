73b26534878e66c3f42cf75701b6cd778a63b399
Yeowoony <dbd04009@naver.com>
2021-06-02 10:27:03 +0900

    add tables for Rest time info

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
           운동(헬스)하는 사람을 위한 웹사이트
         </p>
         <p class="subtitle">
-          <i class="fas fa-dumbbell"></i>운동 
+          <i class="fas fa-dumbbell"></i>운동
           <i class="fas fa-child"></i>부위
         </p>
       </div>
@@ -21,45 +21,66 @@
             <figure class="image">
               <img
                 src="https://mblogthumb-phinf.pstatic.net/20150402_213/nabuilder_1427979616482YttWd_JPEG/chestpress.jpg?type=w2"
-                alt=""
-              />
+                alt="" />
             </figure>
 
-체스트 프레스 머신
+            체스트 프레스 머신
 
-12~15회
+            12~15회
 
-4set
+            4set
 
-<figure class="image">
-  <img
-    src="https://mblogthumb-phinf.pstatic.net/20150402_25/nabuilder_1427979790619UF40c_JPEG/incline.jpg?type=w2"
-    alt=""
-  />
-</figure>
+            <figure class="image">
+              <img
+                src="https://mblogthumb-phinf.pstatic.net/20150402_25/nabuilder_1427979790619UF40c_JPEG/incline.jpg?type=w2"
+                alt="" />
+            </figure>
 
-인클라인 체스트 프레스 
+            인클라인 체스트 프레스
 
-머신
+            머신
 
-12~15회
+            12~15회
 
-4set
+            4set
 
-<figure class="image">
-  <img
-    src="https://mblogthumb-phinf.pstatic.net/20150402_108/nabuilder_1427980211726hUOz2_JPEG/butterfly.jpg?type=w2"
-    alt=""
-  />
-</figure>
+            <figure class="image">
+              <img
+                src="https://mblogthumb-phinf.pstatic.net/20150402_108/nabuilder_1427980211726hUOz2_JPEG/butterfly.jpg?type=w2"
+                alt="" />
+            </figure>
 
-버터플라이 머신
+            버터플라이 머신
 
-12~15회
+            12~15회
 
-3set
+            3set
 
           </div>
+        </article>
+        <article class="notification">
+          <table class="table is-striped is-hoverable is-fullwidth">
+            <thead>
+              <tr>
+                <th>분류</th>
+                <th>Rest Time</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>30초 ~ 60초</td>
+              </tr>
+              <tr class="is-selected">
+                <td>2</td>
+                <td>60초 ~ 90초</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>90초 ~ 120초</td>
+              </tr>
+            </tbody>
+          </table>
         </article>
       </div>
       <div class="column">
@@ -68,47 +89,68 @@
             <p>등</p>
           </div>
           <div class="message-body">
-              <figure class="image">
-                <img
-                  src="https://mblogthumb-phinf.pstatic.net/20150411_147/nabuilder_1428753832342Kr5m9_JPEG/wide-grip-lat-pulldown1.jpg?type=w2"
-                  alt=""
-                  />
-              </figure>
+            <figure class="image">
+              <img
+                src="https://mblogthumb-phinf.pstatic.net/20150411_147/nabuilder_1428753832342Kr5m9_JPEG/wide-grip-lat-pulldown1.jpg?type=w2"
+                alt="" />
+            </figure>
 
-              랫 풀 다운
+            랫 풀 다운
 
-              12~15회
-              
-              4set
-              
-<figure class="image">
-                <img
-                  src="https://mblogthumb-phinf.pstatic.net/20150411_210/nabuilder_1428755817781RmxDI_JPEG/pull-down-beh-neck2.jpg?type=w2"
-                  alt=""
-                  />
-              </figure>
-              
-              비하인드 넥 풀 다운
-              
-              12~15회
-              
-              4set
-              
-              <figure class="image">
-                <img
-                  src="https://mblogthumb-phinf.pstatic.net/20150405_93/nabuilder_1428203066986Hu4As_JPEG/seated_cable_row.jpg?type=w2"
-                  alt=""
-                  />
-              </figure>
-              
-              시티드 케이블 로우
-              
-              12~15회
-              
-              3set
-              
+            12~15회
+
+            4set
+
+            <figure class="image">
+              <img
+                src="https://mblogthumb-phinf.pstatic.net/20150411_210/nabuilder_1428755817781RmxDI_JPEG/pull-down-beh-neck2.jpg?type=w2"
+                alt="" />
+            </figure>
+
+            비하인드 넥 풀 다운
+
+            12~15회
+
+            4set
+
+            <figure class="image">
+              <img
+                src="https://mblogthumb-phinf.pstatic.net/20150405_93/nabuilder_1428203066986Hu4As_JPEG/seated_cable_row.jpg?type=w2"
+                alt="" />
+            </figure>
+
+            시티드 케이블 로우
+
+            12~15회
+
+            3set
+
 
           </div>
+        </article>
+        <article class="notification">
+          <table class="table is-striped is-hoverable is-fullwidth">
+            <thead>
+              <tr>
+                <th>분류</th>
+                <th>Rest Time</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>30초 ~ 60초</td>
+              </tr>
+              <tr class="is-selected">
+                <td>2</td>
+                <td>60초 ~ 90초</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>90초 ~ 120초</td>
+              </tr>
+            </tbody>
+          </table>
         </article>
       </div>
       <div class="column">
@@ -120,43 +162,60 @@
             <figure class="image">
               <img
                 src="https://mblogthumb-phinf.pstatic.net/20150324_153/nabuilder_1427198770853LDLBx_JPEG/girls-squat-.14.jpg?type=w2"
-                alt=""
-                />
+                alt="" />
             </figure>
-              스쿼트
+            스쿼트
 
- 12~15회
+            12~15회
 
- 4set
+            4set
 
- <figure class="image">
-  <img
-    src="https://t1.daumcdn.net/cfile/tistory/99A92E4D5AD9CAAE36"
-    alt=""
-    />
-</figure>
+            <figure class="image">
+              <img src="https://t1.daumcdn.net/cfile/tistory/99A92E4D5AD9CAAE36" alt="" />
+            </figure>
 
- 레그 익스텐션
+            레그 익스텐션
 
- 12~15회
+            12~15회
 
- 3set
+            3set
 
- <figure class="image">
-  <img
-    src="https://blog.kakaocdn.net/dn/oKOOl/btqz7pzhAue/72rC9wcsIXJC3b76kAuwuk/img.jpg"
-    alt=""
-    />
-</figure>
+            <figure class="image">
+              <img src="https://blog.kakaocdn.net/dn/oKOOl/btqz7pzhAue/72rC9wcsIXJC3b76kAuwuk/img.jpg" alt="" />
+            </figure>
 
- 레그 컬
+            레그 컬
 
- 12~15회
+            12~15회
 
- 3set
+            3set
 
 
           </div>
+        </article>
+        <article class="notification">
+          <table class="table is-striped is-hoverable is-fullwidth">
+            <thead>
+              <tr>
+                <th>분류</th>
+                <th>Rest Time</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>30초 ~ 60초</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>60초 ~ 90초</td>
+              </tr>
+              <tr  class="is-selected">
+                <td>3</td>
+                <td>90초 ~ 120초</td>
+              </tr>
+            </tbody>
+          </table>
         </article>
       </div>
       <div class="column">
@@ -165,46 +224,65 @@
             <p>어깨</p>
           </div>
           <div class="message-body">
-              <figure class="image">
-                <img
-                  src="https://t1.daumcdn.net/cfile/blog/99F43B365B30A63709"
-                  alt=""
-                />
-              </figure>
-              덤벨 숄더프레스
+            <figure class="image">
+              <img src="https://t1.daumcdn.net/cfile/blog/99F43B365B30A63709" alt="" />
+            </figure>
+            덤벨 숄더프레스
 
-              12~15회
-              
-              4set
-              
-              <figure class="image">
-                <img
-                  src="https://img1.daumcdn.net/thumb/R720x0.q80/?scode=mtistory2&fname=http%3A%2F%2Fcfile23.uf.tistory.com%2Fimage%2F99677F335ADB3CC3311565"
-                  alt=""
-                />
-              </figure>
-              
-              덤벨 사이드 레터럴레이즈
-              
-              12~15회
-              
-              3set
-              
-              <figure class="image">
-                <img
-                  src="https://mblogthumb-phinf.pstatic.net/MjAxODA2MTRfMjE4/MDAxNTI4OTQ5MzYzMzc4.5CjISR1T4qInO9qyVHmND5qgJKeHihNNGt2DL7BfIkUg.UymQawZX7AdRWE6rLvzCY-Y-atJeyXGu80tKtsHyfx8g.PNG.skathf22/1.png?type=w800"
-                  alt=""
-                />
-              </figure>
-              
-              벤트 오버 레터럴레이즈
-              
-              12~15회
-              
-              3set
-              
-              
+            12~15회
+
+            4set
+
+            <figure class="image">
+              <img
+                src="https://img1.daumcdn.net/thumb/R720x0.q80/?scode=mtistory2&fname=http%3A%2F%2Fcfile23.uf.tistory.com%2Fimage%2F99677F335ADB3CC3311565"
+                alt="" />
+            </figure>
+
+            덤벨 사이드 레터럴레이즈
+
+            12~15회
+
+            3set
+
+            <figure class="image">
+              <img
+                src="https://mblogthumb-phinf.pstatic.net/MjAxODA2MTRfMjE4/MDAxNTI4OTQ5MzYzMzc4.5CjISR1T4qInO9qyVHmND5qgJKeHihNNGt2DL7BfIkUg.UymQawZX7AdRWE6rLvzCY-Y-atJeyXGu80tKtsHyfx8g.PNG.skathf22/1.png?type=w800"
+                alt="" />
+            </figure>
+
+            벤트 오버 레터럴레이즈
+
+            12~15회
+
+            3set
+
+
           </div>
+        </article>
+        <article class="notification">
+          <table class="table is-striped is-hoverable is-fullwidth">
+            <thead>
+              <tr>
+                <th>분류</th>
+                <th>Rest Time</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr class="is-selected">
+                <td>1</td>
+                <td>30초 ~ 60초</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>60초 ~ 90초</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>90초 ~ 120초</td>
+              </tr>
+            </tbody>
+          </table>
         </article>
       </div>
       <div class="column">
@@ -213,13 +291,10 @@
             <p>유산소</p>
           </div>
           <div class="message-body">
-              <figure class="image">
-                <img
-                  src="https://clroro.com/wp-content/uploads/2020/08/50.webp"
-                  alt=""
-                />
-              </figure>
-              
+            <figure class="image">
+              <img src="https://clroro.com/wp-content/uploads/2020/08/50.webp" alt="" />
+            </figure>
+
             <p>
               유산소성 운동에 대한 루틴은 주 3~4회 / 20분내외가 적당합니다.<br><br>
               요새 유산소성 운동은 효과가 좋은 인터벌 트레이닝, 서킷 트레이닝이 유행하고 있습니다.<br><br>
--- upload.bat
+++ upload.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "add nav & footer"
+git commit -m "add tables for Rest time info"
 git push -u origin master
